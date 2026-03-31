0103924eb3bd436ee5b0024648bc561fcf6c1171
Hesso10 <johannes@hessonpaja.com>
2026-03-31 07:30:29 +0300

    Convert HTML file to React component structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>LTS Strategy AI</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/index.tsx"></script>
-  </body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Make sure you have an App.tsx in the same folder
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
